3c58ae3a91347a726a2c7b8bb0a98f1ab71264e7
Daria Alekseevna <dasha.gugnina98@gmail.com>
2022-07-09 05:25:42 +0300

    решение 1 и 3 задачи

---
+++ task1 (25)/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+int A;
+int B;
+Console.WriteLine("Введите число A и B:");
+while ((!int.TryParse(Console.ReadLine(), out A)) || (!int.TryParse(Console.ReadLine(), out B)))
+{
+    System.Console.WriteLine("Введите корректное значение А и В заново:");
+}
+
+
+    double result = Math.Pow(A, B);
+Console.WriteLine($"{A} ^ {B} = {result}");
+
+// еще вариант через цикл
+/* int result = A;
+int i = 1;
+while (i < B)
+{
+    result *= A;
+    i++;
+}
+
+Console.WriteLine($"{A} ^ {B} = {result}"); */
---
+++ task3 (29)/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        array[index] = new Random().Next(1, 50);
+        index++;
+    }
+
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(array[position] + " ");
+        position++;
+    }
+}
+
+int[] array = new int [8]; //определили массив из 8 эл-тов
+
+FillArray(array); //вызвали функцию заполнения массива
+PrintArray(array); //вызвали функцию печати массива

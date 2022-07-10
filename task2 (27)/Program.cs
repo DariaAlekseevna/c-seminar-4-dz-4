af52951f81608b109c0c3665e90c8fa3ee63a8f7
Daria Alekseevna <dasha.gugnina98@gmail.com>
2022-07-10 08:44:24 +0300

    решение второй задачи

--- task2 (27)/Program.cs
+++ task2 (27)/Program.cs
@@ -4,5 +4,24 @@
 82 -> 10
 9012 -> 12 */
 
+int N;
+Console.WriteLine("Введите число:");
+while (!int.TryParse(Console.ReadLine(), out N))
+{
+    System.Console.WriteLine("Введите корректное значение:");
+}
+int SumDigitsInNumber(int N)
+{
+    int sum = 0;
+    while (N > 0)
+    {
+        sum = sum + (N % 10);
+        N /= 10;
+    }
+    return sum;
+}
+
+
+ System.Console.WriteLine("Сумма цифр в числе равна:" + SumDigitsInNumber(N));
 
 

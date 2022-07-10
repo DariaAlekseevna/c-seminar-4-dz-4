fbe6afed33a90892d27e2f10cd5300d516e247d7
Daria Alekseevna <dasha.gugnina98@gmail.com>
2022-07-10 09:10:40 +0300

    немного поменяла решение 3ей задачи

--- task3 (29)/Program.cs
+++ task3 (29)/Program.cs
@@ -3,7 +3,9 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33] */
 
-void FillArray(int[] array)
+// слишком размытое условие задачи и примеры не подходят под условие, если делать как написано в условие задачи, то решение ниже
+
+/* void FillArray(int[] array)
 {
     int length = array.Length;
     int index = 0;
@@ -26,7 +28,48 @@ void PrintArray(int[] array)
     }
 }
 
-int[] array = new int [8]; //определили массив из 8 эл-тов
+int[] array = new int [8]; //определила массив из 8 эл-тов
 
-FillArray(array); //вызвали функцию заполнения массива
-PrintArray(array); //вызвали функцию печати массива
+FillArray(array); 
+PrintArray(array);  */
+
+//если делать как вы говорили на семинаре, то есть пользователь сам вводит числа массива
+
+void FillArray(int[] array)
+{
+    int index = 0;
+    while (index < 8)
+    {
+        Console.Write("введите элемент массива:");
+         array[index] =  int.Parse(Console.ReadLine());
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int position = 0;
+    while (position < 8)
+    {
+        if (position == 0)
+        {
+            Console.Write("[" + col[position]);
+        }
+        else if (position == 7)
+        {
+            Console.Write(col[position] + "]");
+        }
+        else
+        {
+            Console.Write(col[position] + ", ");
+        }
+        position++;
+    }
+}
+
+int[] array = new int [8];
+
+
+FillArray(array); //вызвала функцию заполнения массива
+Console.WriteLine("массив из восьми элементов:");
+PrintArray(array); //вызвала функцию печати массива
